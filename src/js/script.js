983dd2b8a96c5f9dbaf87c6e067fd87e40ac9fdf
lanceferr <lanceferrer1122@gmail.com>
2025-02-14 23:26:29 +0800

    new profiles, threads are made use

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,13 @@
 document.querySelectorAll('.post-interactions button').forEach(button => {
     button.addEventListener('click', function() {
+        const postNumber = this.getAttribute('data-post-number');
         const buttonText = this.innerText;
         if (buttonText.includes('👍')) {
             let votes = parseInt(buttonText.split(' ')[1]);
             this.innerText = `👍 ${votes + 1}`;
+            console.log(`Post ${postNumber} upvoted`);
         } else if (buttonText.includes('💬')) {
-            alert("This will show comments (not implemented in this version).");
+            window.location.href = `thread${postNumber}.html`;
         }
     });
 });
